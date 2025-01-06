a891dde82b6b99a733ca092867553d1e8d97591a
Kiozy30 <23020510@myrp.edu.sg>
2025-01-06 23:58:58 +0800

    3rd commit

---
+++ L8HW/Data.js
@@ -0,0 +1,22 @@
+const datasource = [
+    {
+        title: "Spilled Water",
+        isbn: "123456789",
+        copiesOwned: 69,
+        imageUrl: "https://m.media-amazon.com/images/I/41C5UIWsfdL._AC_UF1000,1000_QL80_.jpg",
+    },
+    {
+        title: "The love hypothesis",
+        isbn: "987654321",
+        copiesOwned: 42,
+        imageUrl: "https://m.media-amazon.com/images/I/71QDhHvv7wL._AC_UF1000,1000_QL80_.jpg",
+    },
+    {
+        title: "The love story",
+        isbn: "987654321",
+        copiesOwned: 20,
+        imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p2259_p_v10_aa.jpg",
+    }
+];
+
+export { datasource };
